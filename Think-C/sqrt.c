2b5c4bd0d9f9ffe851657db4bd6d990df426c0ca
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-05-06 19:51:17 -0500

    More work on think C exercises

---
+++ Think-C/sqrt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+
+int main (void)
+{
+	double num = 9;
+	double guess = num / 2;
+	double xy, xx,xt;
+	short flag = TRUE;
+
+	while (flag)
+	{
+		xy = (guess + num/guess)/2;
+
+		if
+	}
+
+	return EXIT_SUCCESS;
+}
