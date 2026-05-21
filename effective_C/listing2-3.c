6a0be9c553f3bfb98c45f33e57c23864851b565f
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2026-05-21 14:52:06 -0500

    Learned to use declare and dereference pointers

---
+++ effective_C/listing2-3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap (int *pa,int *pb) {
+  int t = *pa;
+  *pa = *pb;
+  *pb = t;
+  printf("swap: a = %d, b = %d\n",*pa,*pb);
+}
+
+int main () {
+  int a = 21;
+  int b = 17;
+  swap(&a,&b);
+  printf("main: a = %d, b = %d\n",a,b);
+  return EXIT_SUCCESS;
+}
